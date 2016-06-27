d74fd0b0aa0f30fda78da7e10a8a4b518319c7b1
Brian Roth <viomonger@gmail.com>
2016-06-26 22:00:58 -0700

    Identify and store data for photon by ID

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 
 var http = require('http');
 
-var myStatus = 'no data';
+var spa_sw_arm = 'no data';
+var spa_hw_arm = 'no data';
+var spa_res = ['no data','no data','no data','no data','no data','no data','no data','no data'];
+var spa_wifi_rssi = 'no data';
 
 http.createServer(function(request, response) {
   request.on('error', function(err) {
@@ -18,7 +21,21 @@ http.createServer(function(request, response) {
       body.push(chunk);
     }).on('end', function() {
       body = Buffer.concat(body).toString();
-      myStatus = body;
+      myStatus = JSON.stringify(request.headers);
+      if (request.headers.id === '210043000347343138333038') {
+        spa_sw_arm = request.headers.sw_arm;
+        spa_hw_arm = request.headers.hw_arm;
+        spa_res[0] = request.headers.r0;
+        spa_res[1] = request.headers.r1;
+        spa_res[2] = request.headers.r2;
+        spa_res[3] = request.headers.r3;
+        spa_res[4] = request.headers.r4;
+        spa_res[5] = request.headers.r5;
+        spa_res[6] = request.headers.r6;
+        spa_res[7] = request.headers.r7;
+        spa_wifi_rssi = request.headers.wifi_rssi;
+      }
+      //myStatus = body;
       response.end();
     })
   } else if (request.method === 'GET' && request.url === '/status') {
@@ -28,7 +45,14 @@ http.createServer(function(request, response) {
     }).on('end', function() {
       body = Buffer.concat(body).toString();
       response.write("I know it's illegal, but it's the weekend!\n\n");
-      response.end(myStatus);
+      response.write("Shemale Porn Addiction:\n");
+      response.write("HW Arm: " + spa_sw_arm + "\n");
+      response.write("SW Arm: " + spa_hw_arm + "\n");
+      for (i = 0; i < 8; i++) {
+        response.write("Res"+String(i)+": " + spa_res[i] + "\n");
+      }
+      response.write("WIFI RSSI: " + spa_wifi_rssi + "\n");
+      response.end();
     })
   } else {
     response.statusCode = 404;
